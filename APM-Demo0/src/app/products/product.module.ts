cd7acc3f8c05afb20b4f03e76419bb8a8ead6960
mwangangievans <muindemwangangi2@gmail.com>
2022-10-05 12:54:59 +0300

    state

---
+++ APM-Demo0/src/app/products/product.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { ProductShellComponent } from './product-shell/product-shell.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { StoreModule } from '@ngrx/store';
+
+const productRoutes: Routes = [
+  { path: '', component: ProductShellComponent }
+];
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild(productRoutes),
+    StoreModule.forFeature('Products',{})
+  ],
+  declarations: [
+    ProductShellComponent,
+    ProductListComponent,
+    ProductEditComponent
+  ]
+})
+export class ProductModule { }
